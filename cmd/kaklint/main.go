87e81aa4dc2ced2b72e734b059cfe2374ffb6428
mzanibelli <mzanibelli@gmail.com>
2021-07-31 13:30:05 +0200

    More idiomatic main()

--- cmd/kaklint/main.go
+++ cmd/kaklint/main.go
@@ -7,21 +7,15 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 3 {
-		usage()
-	}
-
-	if err := kaklint.Default.Lint(os.Args[1], os.Args[2]); err != nil {
-		exit(err)
+	if err := run(os.Args...); err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %v\n", os.Args[0], err)
+		os.Exit(1)
 	}
 }
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s FILETYPE FILE\n", os.Args[0])
-	os.Exit(1)
-}
-
-func exit(err error) {
-	fmt.Fprintf(os.Stderr, "%s: %v", os.Args[0], err)
-	os.Exit(1)
+func run(args ...string) error {
+	if len(args) < 3 {
+		return fmt.Errorf("usage: %s FILETYPE FILE", args[0])
+	}
+	return kaklint.Default.Lint(args[1], args[2])
 }
